7c960df446ff5f921e0488690cada17d8013d989
Avery Myers <averyjmyers@hotmail.com>
2023-07-27 14:34:38 -0700

    starting files complete

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const uuid = require('uuid');
+
+
+app.use("/api", apiRouter)   
+app.use("/html", htmlRouter)
+
+const apiRouter = require("./public/routes/apiRoutes")
+const htmlRouter = require("./public/routes/htmlRoutes")
+
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(expresss.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
+
+
+
+
 
