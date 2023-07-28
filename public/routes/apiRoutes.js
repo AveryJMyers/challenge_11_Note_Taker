2e4f6ee67d6e894497fa1c1164601ae16cee11f0
Avery Myers <averyjmyers@hotmail.com>
2023-07-27 17:35:31 -0700

    working, no delte function

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,7 +1,37 @@
-const express = require("express")
-const router = express.Router()
-const fs = require("fs")
-const path = require("path")
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
 
+router.get('/notes', (req, res) => {
+  try {
+    const notesData = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
+    const notes = JSON.parse(notesData);
+    res.json(notes);
+  } catch (err) {
+    console.error('Error reading notes data:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
+router.post('/notes', (req, res) => {
+  try {
+    const notesData = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
+    const notes = JSON.parse(notesData);
+    const newNote = {
+      id: uuid.v4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(notes));
 
+    res.json({ message: 'Note added successfully!', note: newNote });
+  } catch (err) {
+    console.error('Error saving note:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,35 +2,36 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
-
-app.use("/api", apiRouter)   
-app.use("/html", htmlRouter)
-
-const apiRouter = require("./public/routes/apiRoutes")
-const htmlRouter = require("./public/routes/htmlRoutes")
-
-
+const apiRouter = require("./public/routes/apiRoutes");
+const htmlRouter = require("./public/routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+
+app.use("/api", apiRouter);
+app.use("/html", htmlRouter);
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname,'public', 'routes', 'index.html'));
+   console.log('get /')
+ });
+
+ app.get('/html', (req, res) => {
+  res.sendFile(path.join(__dirname,'public', 'routes', 'index.html'));
+  console.log('get html')
 });
 
+
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'public', 'routes', 'notes.html'));
+  console.log('get /notes')
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
-
-
-
-
-
-
+  console.log(`Server running on http://localhost:${PORT}`);
+});
